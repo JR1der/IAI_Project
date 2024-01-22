9274e8d62cdd4efa16e1d9aee6a199142ce8fe96
IvanOvchynnikov <ovchinic76@gmail.com>
2024-01-22 22:43:53 +0100

    Cur

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+import sparql_request
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route('/', methods=["POST", "GET"])
+def main_page():  # put application's code here
+    if request.method == "POST":
+        data = request.form.get('input_movie')
+        return(sparql_request.results_dict)
+        return render_template("index.html", data=data)
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
