55cec39b76d9a085c2c6061ec30de6cb78c752c7
IvanOvchynnikov <ovchinic76@gmail.com>
2024-01-28 17:57:33 +0100

    Dayana

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import time
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from nlp_search_titles import Searcher
 
 app = Flask(__name__)
@@ -8,38 +8,35 @@ app = Flask(__name__)
 import sparql_request
 import nlp_search_movies
 
-given_film = ""
-found_movies = []
-matches = []
+app.secret_key = 'hello'
 
 
 @app.route('/', methods=["POST", "GET"])
 def main_page():  # put application's code here
-    global matches
-    global given_film
-    global found_movies
 
     if request.method == "POST":
-        given_film = request.form.get('input_movie')
-        if given_film:
-            matches = Searcher().search_words(given_film, list(sparql_request.movies.keys()), max_operations=5)
+        session.pop("matches", [])
+        session.pop("found_movies", [])
+        session.pop("given_film", "")
+        session["given_film"] = request.form.get('input_movie')
+        if session["given_film"]:
+            session["matches"] = Searcher().search_words(session["given_film"], list(sparql_request.movies.keys()),
+                                                         max_operations=5)
             return redirect('/')
         else:
             return redirect('/')
     else:
-        print(found_movies)
+        given_film = session.get("given_film", "")
+        matches = session.get("matches", [])
+        found_movies = session.get("found_movies", [])
         return render_template("index.html", given_film=given_film, movies=matches, found_movies=found_movies)
 
 
 @app.route('/search/<film>', methods=["POST", "GET"])
 def film_select(film):
-    global matches
-    global given_film
-    global found_movies
-
     if request.method == "POST":
         print(film)
-        found_movies = nlp_search_movies.search_movie(film, sparql_request.movies)
+        session["found_movies"] = nlp_search_movies.search_movie(film, sparql_request.movies)
     return redirect('/')
 
 
